6e4e27e589d2facadb9206d5b10157c81c55635f
smperk <smperk71@gmail.com>
2014-07-29 13:37:52 -0400

    built nav and added welcome controller and set root

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+
+  def faq
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Enterpriseape::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/pricing"
+  get "welcome/features"
+  get "welcome/faq"
+  get "welcome/contact"
   resources :invoices
 
-  root to: 'invoices#index'
+  root to: 'welcome#index'
 end
